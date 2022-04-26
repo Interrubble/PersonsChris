494961552380a0bffe00602989420fa26ec9ea34
Interrubble <delagarzachris@icloud.com>
2022-04-26 14:09:51 -0700

    fix

--- server.js
+++ server.js
@@ -12,29 +12,6 @@ const allRoutes = require("./routes")
 app.use(allRoutes);
 
 
-app.post("/books",(req,res)=>{
-    fs.readFile("./data/books.json","utf-8",(err,data)=>{
-        if(err){
-            throw err
-        } else {
-            
-            const books = JSON.parse(data);
-            console.log(req.body)
-            books.push(req.body)
-            fs.writeFile("./data/books.json",JSON.stringify(books,null,2),(err,data)=>{
-                if(err){
-                    throw err
-                }
-                else {
-                    res.json(books)
-                }
-            })
-        }
-    })
-})
-
-
-
 app.listen(PORT,()=>{
     console.log("listening to port " + PORT)
 })
